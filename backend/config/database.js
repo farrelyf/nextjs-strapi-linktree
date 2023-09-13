4a4d7c24d5597e934cbd4b0d411e1766ffedaaf5
farrelyf <farrely.firenza@ui.ac.id>
2023-09-13 09:25:10 +0700

    revised env production in config file

--- backend/config/database.js
+++ backend/config/database.js
@@ -99,26 +99,3 @@ module.exports = ({ env }) => {
     },
   };
 };
-
-// path: ./config/env/production/database.js
-
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
-    },
-  };
-};
